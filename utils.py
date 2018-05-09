000d1364a26975953b291c4cabda6d5e13da7d33
georg_un <onkovic@gmail.com>
2018-05-09 15:06:13 +0200

    Add pattern matching function

---
+++ utils.py
@@ -0,0 +1,22 @@
+import numpy as np
+from _helper import _check_input_array
+from type_ops import is_type_homogeneous
+from type_ops import get_contained_types
+
+
+def match_by_pattern(input_array, patterns):
+
+    _check_input_array(input_array)
+
+    if type(patterns) is not list:
+        patterns = [patterns]
+
+    if not is_type_homogeneous(input_array, verbose=False):
+        raise TypeError("Input array contains multiple types ({0}) but must be type homogeneous.".format(
+            get_contained_types(input_array, unique=True, as_string=True)
+        ))
+
+    matched_array = [value in patterns for value in input_array]
+
+    return matched_array
+
