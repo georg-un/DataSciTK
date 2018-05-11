163f26fd8e96573490eb3166db9910c93eb36eed
georg_un <onkovic@gmail.com>
2018-05-12 00:43:13 +0200

    Add optimization, polinomials and multiplicative terms

--- brute_force_regressions.py
+++ brute_force_regressions.py
@@ -2,20 +2,21 @@ import numpy as np
 import statsmodels.api as sm
 import pandas as pd
 from itertools import combinations
+import warnings
+
+from _helper import _check_pandas_dataframe_nd
+from utils import contains_negatives_or_zero
 
 
-def brute_force_logit(y, X, variables, benchmark_criterion, keep_best_n=5, show_warning=True):
-    # TODO: implement feature for automatic generation of multiplicative terms
-    # TODO: implement feature for automatic generation of polynomials
-    # TODO: implement some kind of optimization to enhance performance
-
+def brute_force_logit(y, X, variables, benchmark_criterion, keep_best_n=5, max_exponent=1, max_root=1,
+                      include_log=False, include_interactions=False, alpha=0.1, optimization=True, verbose=True):
     """
     Brute force a model with a logit regression. Create all possible model specifications of the given variables and
     run a logit regression for each one. Find the n best performing regressions in terms of the benchmark criterion
     and return them.
 
     e.g.
-    $ brute_force_logit(y=y, X=X, variables=['a', 'b', 'c'], benchmark_criterion='aic', keep_best_n=10)
+    $ brute_force_logit(y=y, X=X, variables=['a', 'b', 'c'], benchmark_criterion='aic', keep_best_n=10, max_exponent=3)
     will run a logit regression for each possible variable combination
     (y ~ a;  y ~ b;  y ~ c;  y ~ a + b;  y ~ a + c;  y ~ b + c;  y ~ a + b + c)
     and measure each AIC. Subsequently, it will return the 10 best performing regressions as dictionaries.
@@ -25,11 +26,17 @@ def brute_force_logit(y, X, variables, benchmark_criterion, keep_best_n=5, show_
     :param variables:               List of strings. Contains the column names of the independent variables or modifications e.g. 'np.log(some_column)'
     :param benchmark_criterion:     String. Either 'aic' or 'bic'.
     :param keep_best_n:             Integer. Number of best regressions to return.
-    :param show_warning:            True or False. If False than no warning will be printed for a high number of variables.
+    :param max_exponent:            Integer. Defines the highest exponents to check for each variable.
+    :param max_root:                Integer. Defines the highest roots to check for each variable.
+    :param include_log:             True or False. Defines if the logarithm should be included for each variable.
+    :param include_interactions:    True or False. Defines if interactions between the variables should be included.
+    :param alpha:                   Float. Must be between 0 and 1. Defines the significance level for the optimization.
+    :param optimization:            True or False. Defines if only significant variables should be included (highly recommended).
 
     :return:                        Dictionary of dictionaries. Contains the n best performing regressions.
 
     """
+    # TODO: checks
 
     if type(y) is not pd.DataFrame:
         raise TypeError("Object for parameter 'y' must be of type pd.DataFrame but is currently of the type {0}.".format(
@@ -71,31 +78,40 @@ def brute_force_logit(y, X, variables, benchmark_criterion, keep_best_n=5, show_
     elif keep_best_n < 1:
         raise TypeError("Argument for 'keep_best_n' is smaller than 1. Please use a value of 1 or larger.")
 
-    if type(show_warning) is not bool:
-        raise TypeError("Value for parameter 'show_warning' must be of type boolean (True or False).")
-
-    # Print warning if there are too many variables
-    if len(variables) > 7 and show_warning:
-        print("Warning: Your variables list contains {0} variables. This will result in {1} regressions and may take some time.".format(
-            len(variables),
-            2**len(variables)
-        ))
-
-    # Set column name for y if it is not set
-    if y.columns.name is None:
-        y.columns.name = 'y'
-
     # Concatenate X and y into one data frame
     data_frame = pd.concat([y, X], axis=1)
 
+    # Set column name for dependent variable if it is not already set
+    if data_frame.columns[0] == 0:
+        data_frame.rename(columns={0: 'y'}, inplace=True)
+
+    # Get names of columns with all values above zero
+    columns_larger_zero = []
+    for column in data_frame:
+        if not contains_negatives_or_zero(data_frame[column]):
+            columns_larger_zero.append(column)
+
     # Get model specifications
-    regressions = _get_model_specifications(y.columns.name, variables)
+    regressions = _get_model_specifications(data=data_frame, regression_type='logit', y_name=data_frame.columns[0],
+                                            variables=variables, max_exponent=max_exponent, max_root=max_root,
+                                            include_log=include_log, include_interactions=include_interactions,
+                                            alpha=alpha, optimization=optimization,
+                                            non_negative_columns=columns_larger_zero,
+                                            columns_larger_zero=columns_larger_zero)
 
     # Perform for each model specification a regression
     for regression in regressions:
-
         model = sm.formula.logit(formula=regressions[regression]['formula'], data=data_frame)
-        regressions[regression]['fitted'] = model.fit()
+        try:
+            # Ignore warnings from failed convergence
+            with warnings.catch_warnings():
+                warnings.filterwarnings('ignore')
+                # Try to fit model
+                regressions[regression]['fitted'] = model.fit(disp=0)  # disp=0 to suppress convergence messages being printed
+        except Exception:
+            if verbose:
+                print("Model skipped:", regressions[regression]['formula'])
+            regressions[regression]['fitted'] = None
 
     # Get best indices n regressions
     best_regressions = _get_best_n_regressions(regressions, keep_best_n, benchmark_criterion)
@@ -108,16 +124,83 @@ def brute_force_logit(y, X, variables, benchmark_criterion, keep_best_n=5, show_
     return output_dict
 
 
-def _get_model_specifications(y_name, variables):
+def _get_model_specifications(data, regression_type, y_name, variables, max_exponent=1, max_root=1, include_log=False,
+                              include_interactions=False, alpha=0.1, optimization=True, non_negative_columns=None,
+                              columns_larger_zero=None):
     """
     Creates the regressions-dictionary and fills it with all possible model specifications for the given variables.
 
-    :param y_name:              String. The name of the dependend variable
+    :param y_name:              String. The name of the dependent variable
     :param variables:           List of strings. Contains the variable names
 
     :return:                    Dictionary of dictionaries. Containing all possible model specifications
     """
 
+    # TODO: Checks, description
+
+    # Get exponential variables
+    additional_variables = []
+    for exponent in range(2, max_exponent+1):
+        for variable in variables:
+            new_variable = 'np.power(' + str(variable) + ', ' + str(exponent) + ')'
+            # If optimization is enabled, keep variable only if it is relevant
+            if optimization:
+                if is_relevant_variable(data=data, target_name=y_name, variable_name=new_variable,
+                                        regression_type=regression_type, alpha=alpha):
+                    additional_variables.append(new_variable)
+            else:
+                additional_variables.append(new_variable)
+
+    # Get root variables
+    for root in range(2, max_root+1):
+        for variable in variables:
+            if variable in non_negative_columns:
+                new_variable = 'np.power(' + str(variable) + ', -' + str(float(root)) + ')'
+                # If optimization is enabled, keep variable only if it is relevant
+                if optimization:
+                    if is_relevant_variable(data=data, target_name=y_name, variable_name=new_variable,
+                                            regression_type=regression_type, alpha=alpha):
+                        additional_variables.append(new_variable)
+                else:
+                    additional_variables.append(new_variable)
+
+    # Get log variables
+    if include_log:
+        for variable in variables:
+            if variable in columns_larger_zero:
+                new_variable = 'np.log(' + str(variable) + ')'
+                # If optimization is enabled, keep variable only if it is relevant
+                if optimization:
+                    if is_relevant_variable(data=data, target_name=y_name, variable_name=new_variable,
+                                            regression_type=regression_type, alpha=alpha):
+                        additional_variables.append(new_variable)
+                else:
+                    additional_variables.append(new_variable)
+
+    # Get multiplicative interactions
+    if include_interactions:
+        variable_combinations = combinations(variables, 2)
+        for combination in variable_combinations:
+            new_variable = combination[0] + ' : ' + combination[1]
+            # If optimization is enabled, keep variable only if it is relevant
+            if optimization:
+                if is_relevant_variable(data=data, target_name=y_name, variable_name=new_variable,
+                                        regression_type=regression_type, alpha=alpha):
+                    additional_variables.append(new_variable)
+            else:
+                additional_variables.append(new_variable)
+
+    # Remove original variables which are not significant
+    if optimization:
+        for variable in variables:
+            if not is_relevant_variable(data=data, target_name=y_name, variable_name=variable,
+                                        regression_type=regression_type, alpha=alpha):
+                variables.remove(variable)
+
+    # Add additional variables to variables list
+    variables.extend(additional_variables)
+
+    # Set up regressions dictionary
     regressions = {}
     regressions_index = 0
 
@@ -138,6 +221,80 @@ def _get_model_specifications(y_name, variables):
             regressions_index += 1
 
     return regressions
+
+
+def is_relevant_variable(data, target_name, variable_name, regression_type, alpha):
+
+    # TODO: description
+
+    # Define available regressions
+    REGRESSION_TYPES = [
+        'logit', 'poisson', 'glm', 'gls', 'glsar', 'mnlogit', 'negativebinomial', 'ols', 'probit', 'rlm', 'wls'
+    ]
+
+    # Check if inputs are valid
+    _check_pandas_dataframe_nd(data)
+
+    if type(variable_name) is not str:
+        raise TypeError("Argument for 'variable_name' must be a string.")
+
+    if type(target_name) is not str:
+        raise TypeError("Argument for 'target_name' must be a string.")
+
+    if type(regression_type) is not str:
+        raise TypeError("Argument for 'regression_type' must be a string.")
+    elif regression_type not in REGRESSION_TYPES:
+        raise TypeError("'{0}' is not available as regression-type.".format(regression_type))
+
+    if type(alpha) is not float and type(alpha) is not np.float64:
+        raise TypeError("Argument for 'alpha' must be of type float or numpy float64.")
+    elif alpha <= 0.0 or alpha >= 1.0:
+        raise TypeError("Value for 'alpha' must be between 0 and 1.")
+
+    # Make formula
+    formula = target_name + ' ~ ' + variable_name
+
+    # Get regression
+    if regression_type == 'logit':
+        model = sm.formula.logit(formula=formula, data=data)
+    elif regression_type == 'poisson':
+        model = sm.formula.poisson(formula=formula, data=data)
+    elif regression_type == 'glm':
+        model = sm.formula.glm(formula=formula, data=data)
+    elif regression_type == 'gls':
+        model = sm.formula.gls(formula=formula, data=data)
+    elif regression_type == 'glsar':
+        model = sm.formula.glsar(formula=formula, data=data)
+    elif regression_type == 'mnlogit':
+        model = sm.formula.mnlogit(formula=formula, data=data)
+    elif regression_type == 'negativebinomial':
+        model = sm.formula.negativebinomial(formula=formula, data=data)
+    elif regression_type == 'ols':
+        model = sm.formula.ols(formula=formula, data=data)
+    elif regression_type == 'probit':
+        model = sm.formula.probit(formula=formula, data=data)
+    elif regression_type == 'rlm':
+        model = sm.formula.rlm(formula=formula, data=data)
+    elif regression_type == 'wls':
+        model = sm.formula.wls(formula=formula, data=data)
+    else:
+        raise IOError("Regression method '{0}' has not been found.".format(regression_type))
+
+    # Fit model and get p-value
+    try:
+        p_value = model.fit(disp=0).pvalues[1]  # disp=0 to suppress convergence messages being printed
+    except Exception:
+        p_value = np.nan
+
+    # Check p-value and return respective result
+    if pd.isnull(p_value):
+        return False
+    elif p_value >= alpha:
+        return False
+    elif p_value < alpha:
+        return True
+    else:
+        raise IOError("p-value was neither equal alpha nor smaller or larger than alpha: {0}.".format(p_value))
 
 
 def _get_best_n_regressions(regressions, n, criterion):
@@ -185,15 +342,21 @@ def _get_best_n_regressions(regressions, n, criterion):
     for regression in regressions:
         # If list is not yet full -> fill it
         if len(best_n) < n:
-            best_n.append([regression, getattr(regressions[regression]['fitted'], criterion)])
+            try:
+                best_n.append([regression, getattr(regressions[regression]['fitted'], criterion)])
+            except AttributeError:
+                pass
 
         # Else if new criterion is smaller than the largest of the criteria in the list, replace it for the largest
         else:
             best_n.sort(key=lambda x: x[1], reverse=True)
 
-            if getattr(regressions[regression]['fitted'], criterion) < best_n[0][1]:
-                best_n[0] = [regression, getattr(regressions[regression]['fitted'], criterion)]
-            else:
+            try:
+                if getattr(regressions[regression]['fitted'], criterion) < best_n[0][1]:
+                    best_n[0] = [regression, getattr(regressions[regression]['fitted'], criterion)]
+                else:
+                    pass
+            except AttributeError:
                 pass
 
     best_n.sort(key=lambda x: x[1], reverse=False)
--- utils.py
+++ utils.py
@@ -1,22 +1,71 @@
 import numpy as np
+
 from _helper import _check_numpy_array_1d
+from _helper import _check_numpy_array_pandas_series_1d
 from type_ops import is_type_homogeneous
 from type_ops import get_contained_types
 
 
 def match_by_pattern(input_array, patterns):
+    """
+    Find all occurrences in the input array which are defined in patterns. Returns a list of True and False values.
 
-    _check_numpy_array_1d(input_array)
+    :param input_array:         1-dimensional numpy array or pandas Series.
+    :param patterns:            Single value or list of values. Must be of the same type as the values in the input.
 
+    :return:                    1-dimensional numpy array containing True and False values.
+
+    """
+
+    # Make patterns to a list if it is not already one
     if type(patterns) is not list:
         patterns = [patterns]
+
+    # Check if inputs are valid
+    _check_numpy_array_pandas_series_1d(input_array)
 
     if not is_type_homogeneous(input_array, verbose=False):
         raise TypeError("Input array contains multiple types ({0}) but must be type homogeneous.".format(
             get_contained_types(input_array, unique=True, as_string=True)
         ))
 
+    # Get matches
     matched_array = [value in patterns for value in input_array]
 
     return matched_array
 
+
+def contains_negatives_or_zero(input_array):
+    """
+    Checks if the input contains only values larger than 0. Returns True or False.
+
+    :param input_array:         1-dimensional numpy array or pandas Series of data-type float or int.
+    :return:                    True if input contains only values larger than zero. False otherwise.
+    """
+
+    # Check if input is valid
+    _check_numpy_array_pandas_series_1d(input_array)
+
+    if not is_type_homogeneous(input_array, verbose=False):
+        raise TypeError("Input must be type homogeneous but contains multiple types: {0}.".format(
+            get_contained_types(input_array, unique=True, as_string=True)
+        ))
+
+    if type(input_array[0]) is not int and type(input_array[0]) is not float and type(input_array[0]) is not np.float:
+        if type(input_array[0]) is not np.float64 and type(input_array[0]) is not np.int64:
+            raise TypeError("Input must contain integer or float values but contains type {0}.".format(
+                type(input_array[0])
+            ))
+
+    # Count number of negatives
+    zero_or_smaller = input_array[input_array <= 0]
+
+    # Check number of negatives and return respective result
+    if zero_or_smaller.size == 0:
+        return False
+    elif zero_or_smaller.size > 0:
+        return True
+    else:
+        raise IOError("Number of values which are zero or smaller ({0}) was neither 0 nor a number larger than 0.".format(
+            zero_or_smaller
+        ))
