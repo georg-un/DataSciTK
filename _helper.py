3822aaf6849886ff70144621c94b55fc6c2f2b96
georg_un <onkovic@gmail.com>
2018-05-12 23:37:46 +0200

    Add input checks and descriptions

--- _helper.py
+++ _helper.py
@@ -1,4 +1,5 @@
 from numpy import ndarray
+from numpy import int64, float32, float64
 from pandas import DataFrame
 from pandas import Series
 
@@ -13,6 +14,24 @@ def _check_numpy_array_1d(input_array):
         raise TypeError("Parameter 'input_array' must be a numpy array")
     elif input_array.ndim != 1:
         raise TypeError("Parameter 'input_array' must be 1 dimensional")
+
+
+def _check_pandas_dataframe_1d(data, name):
+    """
+    Check if the input is a 1-dimensional pandas DataFrame.
+
+    """
+
+    if type(data) is not DataFrame:
+        raise TypeError("Object for parameter '{0}' must be of type pd.DataFrame but is currently of the type {1}.".format(
+            str(name),
+            type(data)
+        ))
+    elif data.shape[1] != 1:
+        raise TypeError("Data frame for parameter '{0}' does not have the right shape ({1}). It should have a shape of (n, 1).".format(
+            str(name),
+            data.shape
+        ))
 
 
 def _check_pandas_dataframe_nd(data):
@@ -49,6 +68,79 @@ def _check_numpy_array_pandas_dataframe_series_1d(input_array):
         raise TypeError("'input_array' must be a 1 dimensional numpy array or a pandas DataFrame with shape (m, 1) or a 1 dimensional pandas Series.")
     elif type(input_array) is Series and input_array.ndim != 1:
         raise TypeError("'input_array' must be a 1 dimensional numpy array or a pandas DataFrame with shape (m, 1) or a 1 dimensional pandas Series.")
+
+
+def _check_string(data, name):
+    """
+    Check if input is a string.
+
+    """
+    if type(data) is not str:
+        raise TypeError("Argument for parameter '{0}' must be of type string but is currently of type {1}.".format(
+            str(name),
+            type(data)
+        ))
+
+
+def _check_list_of_strings(data, name):
+    """
+    Check if input is a list of strings.
+
+    """
+    if type(data) is not list:
+        raise TypeError("Argument for parameter '{0}' must be a list of strings.".format(
+            str(name)
+        ))
+    else:
+        for element in data:
+            if type(element) is not str:
+                raise TypeError("At least one value in the list for the parameter '{0}' is not of type string: {1}.".format(
+                    str(name),
+                    element
+                ))
+
+
+def _check_integer(data, name):
+    """
+    Check if input is an integer.
+
+    """
+    if type(data) is not int and type(data) is not int64:
+        raise TypeError("Argument for parameter '{0}' must be of type integer but is currently of type {1}.".format(
+            str(name),
+            type(data)
+        ))
+
+
+def _check_boolean(data, name):
+    """
+    Check if input is boolean.
+
+    """
+    if type(data) is not bool:
+        raise TypeError("Argument for parameter '{0}' must be of type boolean but is currently of type {1}.".format(
+            str(name),
+            type(data)
+        ))
+
+
+def _check_float(data, name):
+    """
+    Check if input is of type float.
+
+    """
+    if type(data) is not float and type(data) is not float64 and type(data) is not float32:
+        raise TypeError("Argument for parameter {0} must be of type float but is currently of type {1].".format(
+            str(name),
+            type(data)
+        ))
+
+
+
+
+
+
+
 
 
 def type_as_string(obj):
--- brute_force_regressions.py
+++ brute_force_regressions.py
@@ -4,7 +4,14 @@ import pandas as pd
 from itertools import combinations
 import warnings
 
+from _helper import _check_pandas_dataframe_1d
 from _helper import _check_pandas_dataframe_nd
+from _helper import _check_string
+from _helper import _check_list_of_strings
+from _helper import _check_integer
+from _helper import _check_boolean
+from _helper import _check_float
+
 from utils import contains_negatives_or_zero
 
 
@@ -32,51 +39,52 @@ def brute_force_logit(y, X, variables, benchmark_criterion, keep_best_n=5, max_e
     :param include_interactions:    True or False. Defines if interactions between the variables should be included.
     :param alpha:                   Float. Must be between 0 and 1. Defines the significance level for the optimization.
     :param optimization:            True or False. Defines if only significant variables should be included (highly recommended).
+    :param verbose:                 True or False. Defines if a warning should be printed when a regression is skipped.
 
     :return:                        Dictionary of dictionaries. Contains the n best performing regressions.
 
     """
-    # TODO: checks
 
-    if type(y) is not pd.DataFrame:
-        raise TypeError("Object for parameter 'y' must be of type pd.DataFrame but is currently of the type {0}.".format(
-            type(y)
-        ))
-    elif y.shape[1] != 1:
-        raise TypeError("Data frame for parameter 'y' does not have the right shape ({0}). It should have a shape of (n, 1).".format(
-            y.shape
-        ))
+    # Check if inputs are valid
+    _check_pandas_dataframe_1d(y, 'y')
 
-    if type(X) is not pd.DataFrame:
-        raise TypeError("Object for parameter 'X' must be of type pd.DataFrame but is currently of the type {0}.".format(
-            type(X)
-        ))
-    elif len(X) != len(y):
+    _check_pandas_dataframe_nd(X)
+
+    if len(X) != len(y):
         raise TypeError("Inputs for 'X' and 'y' have a different number of observations. ({0}, {1})".format(
             len(X),
             len(y)
         ))
 
-    if type(variables) is not list:
-        raise TypeError("Argument for parameter 'variables' must be a list, but is currently of type {0}.".format(
-            type(variables)
-        ))
-    else:
-        for variable in variables:
-            if type(variable) is not str:
-                raise TypeError("At least one element of the list for 'variables' is not a string.")
+    _check_list_of_strings(variables, 'variables')
 
-    if type(benchmark_criterion) is not str:
-        raise TypeError("Argument for 'benchmark_criterion' must be of type string, but is currently of type {0}.".format(
-            type(benchmark_criterion)
-        ))
-    elif benchmark_criterion != 'aic' and benchmark_criterion != 'bic':
+    _check_string(benchmark_criterion, 'benchmark_criterion')
+    if benchmark_criterion != 'aic' and benchmark_criterion != 'bic':
         raise TypeError("Argument for 'benchmark_criterion' must be either 'aic' or 'bic'.")
 
-    if type(keep_best_n) is not int:
-        raise TypeError("Argument for 'keep_best_n' is not of type integer.")
-    elif keep_best_n < 1:
+    _check_integer(keep_best_n, 'keep_best_n')
+    if keep_best_n < 1:
         raise TypeError("Argument for 'keep_best_n' is smaller than 1. Please use a value of 1 or larger.")
+
+    _check_integer(max_exponent, 'max_exponent')
+    if max_exponent < 1:
+        raise TypeError("Argument for 'max_exponent' must not be smaller than 1.")
+
+    _check_integer(max_root, 'max_root')
+    if max_root < 1:
+        raise TypeError("Argument for 'max_root' must not be smaller than 1.")
+
+    _check_boolean(include_log, 'include_log')
+
+    _check_boolean(include_interactions, 'include_interactions')
+
+    _check_float(alpha, 'alpha')
+    if alpha <= 0 or alpha >= 1:
+        raise TypeError("Value for 'alpha' must be between 0 and 1.")
+
+    _check_boolean(optimization, 'optimization')
+
+    _check_boolean(verbose, 'verbose')
 
     # Concatenate X and y into one data frame
     data_frame = pd.concat([y, X], axis=1)
@@ -96,7 +104,6 @@ def brute_force_logit(y, X, variables, benchmark_criterion, keep_best_n=5, max_e
                                             variables=variables, max_exponent=max_exponent, max_root=max_root,
                                             include_log=include_log, include_interactions=include_interactions,
                                             alpha=alpha, optimization=optimization,
-                                            non_negative_columns=columns_larger_zero,
                                             columns_larger_zero=columns_larger_zero)
 
     # Perform for each model specification a regression
@@ -104,7 +111,7 @@ def brute_force_logit(y, X, variables, benchmark_criterion, keep_best_n=5, max_e
         model = sm.formula.logit(formula=regressions[regression]['formula'], data=data_frame)
         try:
             # Ignore warnings from failed convergence
-            with warnings.catch_warnings():
+            with warnings.catch_warnings():     # TODO: find out what happens in cases where MLE does not converge
                 warnings.filterwarnings('ignore')
                 # Try to fit model
                 regressions[regression]['fitted'] = model.fit(disp=0)  # disp=0 to suppress convergence messages being printed
@@ -125,18 +132,67 @@ def brute_force_logit(y, X, variables, benchmark_criterion, keep_best_n=5, max_e
 
 
 def _get_model_specifications(data, regression_type, y_name, variables, max_exponent=1, max_root=1, include_log=False,
-                              include_interactions=False, alpha=0.1, optimization=True, non_negative_columns=None,
-                              columns_larger_zero=None):
+                              include_interactions=False, alpha=0.1, optimization=True, columns_larger_zero=None):
     """
     Creates the regressions-dictionary and fills it with all possible model specifications for the given variables.
 
-    :param y_name:              String. The name of the dependent variable
-    :param variables:           List of strings. Contains the variable names
+    :param data:                    n-dimensional pandas DataFrame containing the dependent variable as first column TODO: maybe change this
+    :param regression_type:         String. Defines the regression type.
+    :param y_name:                  String. Defines the column name of the dependent variable.
+    :param variables:               List of strings. Defines the column names of the variables which should be included.
+    :param max_exponent:            Integer larger than 0. Defines the maximum exponent which should be included for each variable.
+    :param max_root:                Integer larger than 0. Defines the maximum root which should be included for each variable.
+    :param include_log:             Boolean. Defines if the natural logarithm should be included for each variable.
+    :param include_interactions:    Boolean. Defines if multiplicative interactions between the original variables should be included.
+    :param alpha:                   Float. Defines the significance level for the optimizations. Only variables which score a p-value
+                                    smaller than alpha in a simple linear model are included in the model specifications.
+    :param optimization:            Boolean. Defines if an optimization should run (highly recommended).
+    :param columns_larger_zero:     List of strings. Log and root are only included for columns which are mentioned in this list.
 
-    :return:                    Dictionary of dictionaries. Containing all possible model specifications
+    :return:                        Dictionary of dictionaries. Contains the formula for each model specification.
+
     """
 
-    # TODO: Checks, description
+    # Define available regressions
+    REGRESSION_TYPES = [
+        'logit', 'poisson', 'glm', 'gls', 'glsar', 'mnlogit', 'negativebinomial', 'ols', 'probit', 'rlm', 'wls'
+    ]
+
+    # Check if inputs are valid
+    _check_pandas_dataframe_nd(data)
+
+    _check_string(regression_type, 'regression_type')
+    if regression_type not in REGRESSION_TYPES:
+        raise TypeError("Argument for parameter 'regression_type' ({0}) is not a valid regression type.".format(
+            regression_type
+        ))
+
+    _check_string(y_name, 'y_name')
+
+    _check_list_of_strings(variables, 'variables')
+
+    _check_integer(max_exponent, 'max_exponent')
+    if max_exponent < 1:
+        raise TypeError("Argument for parameter 'max_exponend' must be 1 or larger.")
+
+    _check_integer(max_root, 'max_root')
+    if max_root < 1:
+        raise TypeError("Argument for parameter 'max_root' must be 1 or larger.")
+
+    _check_boolean(include_log, 'include_log')
+
+    _check_boolean(include_interactions, 'include_interactions')
+
+    _check_float(alpha, 'alpha')
+    if alpha <= 0 or alpha >= 1:
+        raise TypeError("Argument for parameter 'alpha' must be a value between 0 and 1.")
+
+    _check_boolean(optimization, 'optimization')
+
+    if columns_larger_zero is not None:
+        _check_list_of_strings(columns_larger_zero, 'columns_larger_zero')
+    if (include_log or max_root > 1) and (columns_larger_zero is None or np.size(columns_larger_zero) == 0):
+        print("Argument for parameter 'columns_larger_zero' contains no columns. Logarithm and roots will not be included.")
 
     # Get exponential variables
     additional_variables = []
@@ -154,7 +210,7 @@ def _get_model_specifications(data, regression_type, y_name, variables, max_expo
     # Get root variables
     for root in range(2, max_root+1):
         for variable in variables:
-            if variable in non_negative_columns:
+            if variable in columns_larger_zero:
                 new_variable = 'np.power(' + str(variable) + ', -' + str(float(root)) + ')'
                 # If optimization is enabled, keep variable only if it is relevant
                 if optimization:
@@ -224,8 +280,18 @@ def _get_model_specifications(data, regression_type, y_name, variables, max_expo
 
 
 def is_relevant_variable(data, target_name, variable_name, regression_type, alpha):
+    """
+    Runs a simple regression (target ~ variable) and returns true if the p-value is smaller than the given alpha.
 
-    # TODO: description
+    :param data:                    n-dimensional pandas DataFrame. Must contain the dependent and the independent variable.
+    :param target_name:             String. Defines the column name of the dependent variable.
+    :param variable_name:           String. Defines the column name of the independent variable.
+    :param regression_type:         String. Defines the regression type (see REGRESSION_TYPES).
+    :param alpha:                   Float. Defines the significance level.
+
+    :return:                        True if the p-value of the variable is smaller than alpha. False otherwise.
+
+    """
 
     # Define available regressions
     REGRESSION_TYPES = [
@@ -235,20 +301,16 @@ def is_relevant_variable(data, target_name, variable_name, regression_type, alph
     # Check if inputs are valid
     _check_pandas_dataframe_nd(data)
 
-    if type(variable_name) is not str:
-        raise TypeError("Argument for 'variable_name' must be a string.")
+    _check_string(target_name, 'target_name')
 
-    if type(target_name) is not str:
-        raise TypeError("Argument for 'target_name' must be a string.")
+    _check_string(variable_name, 'variable_name')
 
-    if type(regression_type) is not str:
-        raise TypeError("Argument for 'regression_type' must be a string.")
-    elif regression_type not in REGRESSION_TYPES:
+    _check_string(regression_type, 'regression_type')
+    if regression_type not in REGRESSION_TYPES:
         raise TypeError("'{0}' is not available as regression-type.".format(regression_type))
 
-    if type(alpha) is not float and type(alpha) is not np.float64:
-        raise TypeError("Argument for 'alpha' must be of type float or numpy float64.")
-    elif alpha <= 0.0 or alpha >= 1.0:
+    _check_float(alpha, 'alpha')
+    if alpha <= 0.0 or alpha >= 1.0:
         raise TypeError("Value for 'alpha' must be between 0 and 1.")
 
     # Make formula
@@ -322,16 +384,14 @@ def _get_best_n_regressions(regressions, n, criterion):
         if 'fitted' not in regressions[regression].keys():
             raise TypeError("At least one dict for the input of the parameter 'regressions' does not contain the key 'fitted'.")
 
-    if type(n) is not int:
-        raise TypeError("Value for parameter 'n' is not of type integer. Please use an integer value.")
-    elif n <= 0:
+    _check_integer(n, 'n')
+    if n <= 0:
         raise TypeError("Value for parameter 'n' ({0}) is smaller than 1. Please use a value of 1 or larger.".format(
             n
         ))
 
-    if type(criterion) is not str:
-        raise TypeError("Argument for parameter 'criterion' is not of type string.")
-    elif criterion != 'aic' and criterion != 'bic':
+    _check_string(criterion, 'criterion')
+    if criterion != 'aic' and criterion != 'bic':
         raise TypeError("Argument for parameter 'criterion' ({0}) does not define a valid criterion. Please use 'aic' or 'bic' as criterion.".format(
             criterion
         ))
